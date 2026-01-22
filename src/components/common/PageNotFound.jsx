38b7f243fe63316342d615642b4d00d975eb1a2c
pramesh-gawas <prameshgawas11@gmail.com>
2026-01-22 16:30:56 +0530

    add page not found

--- src/components/common/PageNotFound.jsx
+++ src/components/common/PageNotFound.jsx
@@ -16,16 +16,10 @@ export const PageNotFound = () => {
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
-              href="#"
+              href="/shop"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
             >
               Go back home
-            </a>
-            <a
-              href="#"
-              className="text-sm font-semibold text-gray-900 dark:text-white"
-            >
-              Contact support <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </div>
